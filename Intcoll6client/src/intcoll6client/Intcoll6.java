0c76d24e1ecf9bfbbd54dfe2f7948ae65928077a
Jim Krider <tuf24424@temple.edu>
2014-11-13 18:00:26 -0500

    Adding The Files

---
+++ Intcoll6client/src/intcoll6client/Intcoll6.java
@@ -0,0 +1,310 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package intcoll6client;
+import java.io.*;
+
+/**
+ *
+ * @author Jimbo
+ */
+public class Intcoll6 {
+    private int howmany;
+    private btNode c;
+
+
+    public Intcoll6()
+    {
+      c = null;
+      howmany = 0;
+    } 
+    
+    public Intcoll6(int i)
+    {
+      c = null;
+      howmany = 0;
+    }   
+    
+    private static btNode copytree(btNode t)
+    {
+      btNode root = null;
+      if (t != null)
+      {
+         root       = new btNode();
+         root.info  = t.info; 
+         root.left  = copytree(t.left);
+         root.right = copytree(t.right);
+      }
+      return root;
+    }   
+    
+    public void copy(Intcoll6 obj)
+    {
+      if (this != obj)
+      {
+          howmany = obj.howmany;
+          c = copytree(obj.c);  
+      }
+    }   
+    
+    public void insert(int i)
+    {
+      btNode pred = null, p = c;
+      while ((p != null)&&(p.info != i))
+      {
+            pred = p;
+            if (p.info > i) p = p.left; //If i is less then Current node go LEFT
+            else p = p.right;           //Else go right
+      }
+      if (p == null)                    //If the node is null
+      {
+         howmany++; p = new btNode(i, null, null);//create a new one
+        if (pred != null)
+        {
+            if (pred.info > i) pred.left = p;
+            else pred.right = p;
+        }
+        else c = p;
+      }
+    }   
+    
+    public void omit(int i)
+    {
+        btNode p = c, predp = null;
+       while (( p != null )&&( p.info != i ))
+        {
+            predp = p;
+            if (p.info > i) {p = p.left; System.out.println("L");}//Prints L for Left I used it to check 
+            else{ p = p.right; System.out.println("R");}//Prints R for Right I use it to check not really needed
+        }
+       
+        if((p.left == null)&&(p.right == null)){ //Leaf Node
+            if(predp == null){
+               c = null;           
+            }else if(predp.info > p.info){        //Go Left if the Previous or Parent is greater then P
+               predp.left  = null;
+            }else if(predp.info < p.info){           //Else go right
+               predp.right = null;
+            }
+        }else if(p.left == null){
+            if(predp == null){
+               c = p.right;
+            }else if(predp.info > p.info){
+               predp.left  = p.right;
+            }else{
+               predp.right = p.right;
+            }
+        }else if(p.right == null){
+            if(predp == null){
+               c = p.left;
+            }else if(predp.info > p.info){
+               predp.left  = p.left;
+            }else{
+               predp.right = p.left;
+            }
+        }else if((p.left != null)&&(p.right != null)){ 
+            if(predp == null){
+                btNode max = p.left,maxpred = null;
+                while(max.right != null){
+                    maxpred = max;
+                    max = max.right;
+                }           
+            c.info/*<-----*/= max.info;
+            maxpred.right   = max.left;
+            c.right/*<----*/= p.right;
+            c.left/*<-----*/= p.left;
+            }else if(predp.info > p.info){
+                btNode min = p;                
+                while(min.left != null){
+                   min = min.left;
+                }
+               predp.left = min;
+               min.right  = p.right;
+            }else{
+               btNode min = p;
+               while(min.left != null){
+                   min = min.left;
+                }
+              predp.right = min; 
+              min.right = p.right;
+            }
+        }//End of Inner If statements 
+    }//End of Omit Method   
+    
+    public boolean belongs(int i)
+    {
+      btNode p = c;
+      while ((p != null)&&(p.info != i)) 
+      {
+         if (p.info > i) p = p.left;
+         else p = p.right;
+      }
+      return (p != null); 
+    }//End of Belongs Method   
+    
+    public int get_howmany() {return howmany;}//End of get_howmany   
+    
+    public void print(String outname)
+    {
+      try
+      {
+	 PrintWriter outs =new PrintWriter(new FileOutputStream(outname));
+         outs.println("The number of integers is "+howmany);
+         outs.println();
+         printtree(c, outs);
+         outs.close();
+      }catch (IOException ex)
+      {
+      }
+    }//End of print that works with outs
+    
+    public void btSort(Intcoll6 obj)
+    {
+        int[] sort = new int[obj.howmany];   
+        toarray(obj.c,sort,0);//Sorts the tree like insertion sort
+        //quicksort(sort,0,sort.length-1);//Sorts the tree using quick
+        c = sortedArrayToBST(sort,0,sort.length-1);//sets this.c to the sorted tree
+    
+   }
+    
+    private btNode sortedArrayToBST(int[] a, int start, int end)
+    {
+       if(start > end)
+            return null;
+        int mid       = (start + end)/2;
+        btNode root   = new btNode(a[mid], null, null);howmany++;
+        if(mid > start) root.left     = sortedArrayToBST(a,start,mid-1);
+        if(mid < end)   root.right    = sortedArrayToBST(a,mid+1,end);
+       
+       return root;
+   }
+    
+    public static void quicksort(int a[], int l, int h)
+    {
+        int p;
+        if((h - l) > 0){
+            p = partition(a,l,h);
+            quicksort(a,   l, p-1);
+            quicksort(a, p+1, h);
+        }
+    }
+    
+    public static int partition( int data[], int l, int h)
+    {
+        int upper;
+        int lower;
+        int firsthigh;
+        upper = l; lower = h; firsthigh = data[l];
+        while(upper != lower){
+            while((upper < lower)&&(firsthigh >= data[lower]))
+                   lower--;
+                if(upper != lower){
+                   data[upper] = data[lower];
+                }             
+            while((upper < lower)&&(firsthigh <= data[upper]))
+                   upper++;
+                if(upper != lower){
+                   data[lower] = data[upper];
+                }
+          
+        }       
+        data[upper] = firsthigh;
+        return(upper);
+    }
+    
+    private btNode quickSortBST(int a[], int start, int end)
+    {
+       int p; btNode root =null;
+       
+       if((end - start) > 0){
+       p = partition(a,start,end);
+       root = new btNode(a[p], null, null);howmany++;
+       if(p > start)root.left     =  quickSortBST(a,   start, p-1);
+       if(p < end)  root.right    =  quickSortBST(a,     p+1, end);
+           
+        }
+       
+       return root;
+   }//Does not Print correctly, it is weird
+    
+    public void print()
+    {
+      printtree(c);
+      if(c == null){System.out.println(c);}
+    }//End of print    
+    
+    public boolean equals(Intcoll6 obj)
+    {
+      int j; boolean result  = (howmany==obj.howmany);
+      if (result)
+      { 
+         int[] a = new int[howmany]; 
+         int[] b = new int[howmany];
+         toarray(c, a, 0); toarray(obj.c, b, 0);
+
+         j=0;
+         while ((result) && (j<howmany))
+         {
+            result=(a[j]==b[j]); j++;
+         }
+      }
+      return result;
+    }//End of Equals Method   
+    
+    private static void printtree(btNode t, PrintWriter outs)
+    {   
+	if (t!=null)
+	{
+            printtree(t.left, outs);
+            outs.println(t.info );
+            printtree(t.right, outs);
+	}
+   }//End of Printtree method for PrintWriter OUTS   
+    
+    private static void printtree(btNode t)
+    {    
+      if(t != null)
+      {  
+        printtree(t.left);
+        System.out.println(t.info);
+        if(t.right != null)
+            System.out.println("Right " + t.right.info);
+        else
+            System.out.println("Right is null" );
+        if(t.left != null)
+            System.out.println("Left " + t.left.info);
+        else
+            System.out.println("Left is null" );
+        printtree(t.right);
+      } 
+    }//End of Printtree method   
+    
+    private static int toarray(btNode t, int[] a, int i)
+    {
+      int num_nodes = 0;int r=0,l=0;
+      if (t != null)
+      { 
+        num_nodes      = toarray(t.left, a, i);l=i;
+        a[num_nodes+i] = t.info; r=i;
+        num_nodes      = num_nodes + 1 + toarray(t.right, a, num_nodes+i+1);
+       }
+      return num_nodes;
+   }//End of toarray method
+    
+    private static class btNode
+    {
+       int info; btNode left; btNode right;
+
+       private btNode(int s, btNode lt, btNode rt)
+       {
+        info = s; left = lt;   right = rt;  
+       }
+
+       private btNode()
+       {
+        info = 0; left = null; right = null;
+       }
+   }//End of Class btNode
+}
---
+++ Intcoll6client/src/intcoll6client/Intcoll6client.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package intcoll6client;
+import java.util.*;
+/**
+ *
+ * @author Jimbo
+ */
+public class Intcoll6client
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+    Intcoll6 P = new Intcoll6();
+    Intcoll6 Q = new Intcoll6();
+
+    int value; Scanner keyboard=new Scanner(System.in);
+    System.out.println(
+         "Enter a pos integer or neg integer to be inserted or 0 to quit:");
+    /*value=keyboard.nextInt();
+    while(value!=0)
+    {
+       if (value>0) P.insert(value); else Q.insert(-value);
+       System.out.println(
+         "Enter pos integer or neg integer to be inserted or 0 to quit:");
+       value=keyboard.nextInt();
+    }*/
+    System.out.println("\nThe values in collection P are:"); 
+    P.insert(10);
+    P.insert(1);
+    P.insert(9);
+    P.insert(2);
+    P.insert(8);
+    P.insert(3);
+    P.insert(7);
+    P.insert(4);
+    P.insert(6);
+    P.insert(5);
+    Q.btSort(P);
+    System.out.println("\nThe values in collection P after it was omitted are:");
+    P.print();
+    System.out.println("\n");
+    Q.print();
+ 
+  
+    }//End of Main Class
+	
+    
+}
